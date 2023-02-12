12c94e1ace3b71891eda78ed6ec755b0d54187fe
Valentin Hervieu <valentin@hervi.eu>
2023-02-12 23:34:26 +0100

    chore: initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const colors = require('tailwindcss/colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.slate,
+      green: colors.emerald,
+      purple: colors.violet,
+      yellow: colors.amber,
+      pink: colors.pink,
+      red: colors.rose,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
