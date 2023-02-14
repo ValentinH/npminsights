f12cdb4cf9ad7e71ccaa8d7cab6e6872630d9545
Valentin Hervieu <valentin@hervi.eu>
2023-02-14 09:19:46 +0100

    feat: improve mobile layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <head />
-      <body className="h-screen w-full bg-gray-900 text-white">
+      <body className="h-screen w-full overflow-y-auto overflow-x-hidden bg-gray-900 text-white">
         <main className="container mx-auto p-4">{children}</main>
       </body>
     </html>
