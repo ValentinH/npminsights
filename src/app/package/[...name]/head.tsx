0c9396c5fbfdaddee477a60aa4c4cebe94e74505
Valentin Hervieu <valentin@hervi.eu>
2023-02-23 00:09:25 +0100

    feat: improve title meta for packages

--- src/app/package/[...name]/head.tsx
+++ src/app/package/[...name]/head.tsx
@@ -5,6 +5,7 @@ export default function Head({ params }: PackageProps) {
   const packageName = name.map(decodeURIComponent).join('/');
   return (
     <>
+      <title>{`${packageName} - npminsights.com`}</title>
       <meta property="og:image" content={`/api/package/og-image/${packageName}`} />
     </>
   );
